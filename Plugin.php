7cc1717810dc4cf001ad0400f879869cea688b45
Stefan Ninic <ninicstefan94@gmail.com>
2017-09-04 23:21:22 +0200

    Plugin.php got corrupted after unresolved merges, had to undo to the latest version.

--- Plugin.php
+++ Plugin.php
@@ -1,25 +1,14 @@
 <?php namespace Stefan\ImageUploadExtended;
-
-<<<<<<< HEAD
 use Event;
-=======
->>>>>>> 651edf6c355b93a2b550b00626e495a2383737de
 use Backend;
 use System\Classes\PluginBase;
-
 /**
  * ImageUploadExtended Plugin Information File
  */
 class Plugin extends PluginBase
 {
-
-<<<<<<< HEAD
     public $require = ['Raviraj.Rjgallery'];
-
     public $elevated = true;
-=======
-    public $require = ['Raviraj.RJGallery'];
->>>>>>> 651edf6c355b93a2b550b00626e495a2383737de
     /**
      * Returns information about this plugin.
      *
@@ -28,33 +17,24 @@ class Plugin extends PluginBase
     public function pluginDetails()
     {
         return [
-<<<<<<< HEAD
             'name'        => 'Image Upload Extended',
             'description' => 'RJ Gallery extended with image tags',
-=======
-            'name'        => 'stefan.imageuploadextended::lang.plugin.name',
-            'description' => 'stefan.imageuploadextended::lang.plugin.description',
->>>>>>> 651edf6c355b93a2b550b00626e495a2383737de
             'author'      => 'Stefan Ninic',
             'icon'        => 'icon-leaf'
         ];
     }
-
-<<<<<<< HEAD
     public function registerFormWidgets()
     {
         return [
             'Stefan\ImageUploadExtended\formwidgets\ImageUpload' => 'imageupload',
         ];
     }
-
     public function registerComponents()
     {
         return [
             'Stefan\ImageUploadextended\Components\Gallery' => 'extendedGalleryId',
         ];
     }
-
     /**
      * Method used to remove default RJ Gallery field and insert our custom with image tags
      *
@@ -66,11 +46,8 @@ class Plugin extends PluginBase
     {
         /*
         \Raviraj\Rjgallery\Controllers\Galleries::extendFormFields(function($form, $model, $context) {
-
             if(!$form->isNested) {
-
                 $form->removeField('images');
-
                 $form->addFields([
                     'gallery_images' => [
                         'label' => 'Gallery Images',
@@ -81,12 +58,10 @@ class Plugin extends PluginBase
                                     'label' => 'Photo Title',
                                     'type' => 'text',
                                 ],
-
                                 'tags' => [
                                     'label' => 'Photo Tags',
                                     'type' => 'text',
                                 ],
-
                                 'src' => [
                                     'label' => 'Photo',
                                     'type' => 'mediafinder',
@@ -100,59 +75,5 @@ class Plugin extends PluginBase
                 ]);
             }
         });*/
-=======
-    public function registerNavigation()
-    {
-        return [
-            'gallery' => [
-                'label' => 'stefan.imageuploadextended::lang.menu.name',
-                'url'   => Backend::url('stefan/imageuploadextended/galleries'),
-                'icon'        => 'icon-picture-o',
-                'permissions' => ['stefan.imageuploadextended.*'],
-                'order'       => 500,
-
-                'sideMenu' => [
-                    'new_gallery' => [
-                        'label'       => 'stefan.imageuploadextended::lang.menu.new_gallery',
-                        'icon'        => 'icon-plus',
-                        'url'         => Backend::url('stefan/imageuploadextended/galleries/create'),
-                        'permissions' => ['stefan.imageuploadextended.access_galleries']
-                    ],
-                    'galleries' => [
-                        'label'       => 'stefan.imageuploadextended::lang.menu.galleries',
-                        'icon'        => 'icon-file-image-o',
-                        'url'         => Backend::url('stefan/imageuploadextended/galleries'),
-                        'permissions' => ['stefan.imageuploadextended.access_galleries']
-                    ],
-                    'new_category' => [
-                        'label'       => 'stefan.imageuploadextended::lang.menu.new_category',
-                        'icon'        => 'icon-plus',
-                        'url'         => Backend::url('stefan/imageuploadextended/categories/create'),
-                        'permissions' => ['stefan.imageuploadextended.access_galleries']
-                    ],
-                    'categories' => [
-                        'label'       => 'stefan.imageuploadextended::lang.menu.categories',
-                        'icon'        => 'icon-server',
-                        'url'         => Backend::url('stefan/imageuploadextended/categories'),
-                        'permissions' => ['stefan.imageuploadextended.access_categories']
-                    ]
-                ]
-            ],
-        ];
-    }
-
-    public function registerPermissions()
-    {
-        return [
-            'stefan.imageuploadextended.*' => ['tab' => 'stefan.imageuploadextended::lang.plugin.name', 'label' => 'stefan.imageuploadextended::lang.permissions.all']
-        ];
-    }
-
-    public function registerFormWidgets()
-    {
-        return [
-            'Stefan\ImageUploadExtended\formwidgets\ImageUpload' => 'imageupload',
-        ];
->>>>>>> 651edf6c355b93a2b550b00626e495a2383737de
     }
 }
